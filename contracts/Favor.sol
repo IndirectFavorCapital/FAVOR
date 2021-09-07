8007a2e74f83d403c1344b911ee2944321b9c5cd
A-Vlasov <LazyLazyJon@gmail.com>
2021-09-07 23:29:23 +0400

    Add Pausable, Permit, Votes, FlashMinting, Upgradable

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -3,18 +3,33 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
-contract FavorToken is ERC20, Ownable, ReentrancyGuard {
+contract FavorToken is ERC20, Ownable, ReentrancyGuard, Pausable, ERC20Burnable, ERC20Permit, ERC20Votes, ERC20FlashMint {
+
     using SafeERC20 for IERC20;
 
     uint256 immutable private _cap;
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol) ERC20Permit(_name) {
         _cap = 100000000;
         _mint(msg.sender, _totalSupply);
+    }
+	
+	function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
     function mint(uint256 _amount) external onlyOwner {
@@ -24,4 +39,21 @@ contract FavorToken is ERC20, Ownable, ReentrancyGuard {
     function burn(uint256 _amount) external {
         _burn(msg.sender, _amount);
     }
+	
+	function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+	
+	function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+	
 }
