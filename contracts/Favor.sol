bb9efd29e45759ad5d5c923bd9670e6817be7410
tinajpshvl <87702611+tinajpshvl@users.noreply.github.com>
2021-09-09 03:09:36 +0800

    Update Favor.sol
    Added NatSpec comments.

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -16,22 +16,46 @@ contract FavorToken is ERC20, ERC20Capped, ERC20Burnable, ERC20Votes, Ownable, P
 
     using SafeERC20 for IERC20;
 
+    /**
+     * @dev Initializes the contract minting the new tokens for the deployer.
+     * deployer here is the owner of the FavorToken.
+     */
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply, uint256 _cap) ERC20(_name, _symbol) ERC20Capped(_cap) {
         _mint(msg.sender, _totalSupply);
     }
 
+    /**
+     * @dev Pauses all token transfers. Only the owner can call pause().
+     *
+     * See {Pausable-_pause}.
+     */
     function pause() external onlyOwner {
         _pause();
     }
 
+    /**
+     * @dev Unpauses all token transfers. Only the owner can call unpause().
+     *
+     * See {Pausable-_unpause}.
+     */
     function unpause() external onlyOwner {
         _unpause();
     }
 
+    /**
+     * @dev Creates `_amount` new tokens. Only the owner can call mint().
+     *
+     * See {ERC20-_mint}.
+     */
     function mint(uint256 _amount) external onlyOwner {
         _mint(msg.sender, _amount);
     }
 
+    /**
+     * @dev Destroys `_amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
     function burn(uint256 _amount) external {
         _burn(msg.sender, _amount);
     }
@@ -39,6 +63,7 @@ contract FavorToken is ERC20, ERC20Capped, ERC20Burnable, ERC20Votes, Ownable, P
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override {
         super._beforeTokenTransfer(from, to, amount);
     }
+
 
     function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
