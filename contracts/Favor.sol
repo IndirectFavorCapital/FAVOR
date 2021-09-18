8c72e79f916fc17d2e4ba0092f41ec119064917d
A-Vlasov <LazyLazyJon@gmail.com>
2021-09-19 01:16:57 +0400

    Grafity

--- contracts/Favor.sol
+++ contracts/Favor.sol
@@ -9,7 +9,7 @@ pragma solidity 0.8.6;
 //   _| |_  _/ /   \ \_\ ' /   \  `-'  /_| |  \ \_  
 //  |_____||____| |____|\_/     `.___.'|____| |___|     
 
-// https://favor.capital						 
+//  https://favor.capital						 
 						 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
