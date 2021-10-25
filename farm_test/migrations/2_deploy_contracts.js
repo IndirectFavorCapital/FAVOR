2ef00bc7a19a9a04dfb440d7056178103fcacf64
A-Vlasov <LazyLazyJon@gmail.com>
2021-10-25 18:23:29 +0400

    Fix start block number

--- farm_test/migrations/2_deploy_contracts.js
+++ farm_test/migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ const FAVOR_TOKEN = "0x6Edf2d4937FaabeaA5c81302248AbDB722787feB";
 const PANCAKESWAP_FARM = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
 const CAKE_TOKEN = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82";
 const TOKENS_PER_BLOCK = 40;
-const START_BLOCK = 13508103;
+const START_BLOCK = 12080484;
 
 module.exports = async function (deployer, network, accounts) {
 
